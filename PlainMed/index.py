0ba89b68219dac4733dcff0fe50e3e39da3af101
Kiddi <kristinnv12@ru.is>
2015-05-16 16:10:36 +0000

    py3

---
+++ PlainMed/index.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from bs4 import BeautifulSoup
+import urllib.request
+
+app = Flask(__name__)
+
+@app.route('/')
+def myMeds():
+    return render_template('meds.html', data=getMeds())
+
+def getMeds():
+	req = urllib.request.Request('http://lyfjaver.is/lyfjaskra')
+	response = urllib.request.urlopen(req)
+	return response
+
+@app.route('/SignIn')
+def singIn():
+    return render_template('signIn.html')
+
+@app.route('/SignUp')
+def signUp():
+    return render_template('signUp.html')
+
+if __name__ == '__main__':
+    app.run(debug=1)
