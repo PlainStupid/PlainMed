9427f75f352486eb55531601c43df1157b76fec9
Kiddi <kristinnv12@ru.is>
2015-05-16 18:03:30 +0000

    medication template

--- PlainMed/index.py
+++ PlainMed/index.py
@@ -4,17 +4,17 @@ import urllib.request
 
 app = Flask(__name__)
 
-@app.route('/')
-def myMeds():
-    return render_template('meds.html', data=getMeds())
-
 def getMeds():
 	req = urllib.request.Request('http://lyfjaver.is/lyfjaskra')
 	response = urllib.request.urlopen(req)
 	bsoup = BeautifulSoup(response)
 	tables = bsoup.findAll('td', {'class': 'row1'})
 
-	return tables
+	return [i.string for i in tables]
+
+@app.route('/')
+def myMeds():
+    return render_template('meds.html', data=getMeds())
 
 @app.route('/SignIn')
 def singIn():
@@ -23,6 +23,14 @@ def singIn():
 @app.route('/SignUp')
 def signUp():
     return render_template('signUp.html')
+
+@app.route('/Med/<medication>')
+def med(medication):
+	print(getMeds())
+	if medication in getMeds():
+		return render_template('med.html', med=medication)
+	else:
+		return render_template('404.html', med=medication)
 
 if __name__ == '__main__':
     app.run(debug=1)
