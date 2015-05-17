1674b3106588ee27e0260353f131b63f5010151e
Kiddi <kristinnv12@ru.is>
2015-05-17 01:31:29 +0000

    form

--- PlainMed/app/mod_index/controllers.py
+++ PlainMed/app/mod_index/controllers.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template, abort, request
+from flask import Blueprint, render_template, abort, request, redirect, url_for
 from flask.ext.login import LoginManager, current_user, login_user, logout_user, login_required
 from bs4 import BeautifulSoup
 import urllib.request
+from wtforms import Form, IntegerField, TextField, PasswordField, validators, SelectField
 
 mod_index = Blueprint("index", __name__, url_prefix="/", template_folder="templates")
 
-
+# Helper function for scraping lyfjaver for medications
 def getMeds():
 	req = urllib.request.Request('http://lyfjaver.is/lyfjaskra')
 	response = urllib.request.urlopen(req)
@@ -14,20 +15,35 @@ def getMeds():
 
 	return [i.string for i in tables]
 
+# Home Page - list of medications
 @mod_index.route("/")
 @login_required
 def index():
     return render_template('meds.html', data=getMeds())
 
-@mod_index.route('Med/<medication>')
+# Page for each medication, for registering meds to your own list
+@mod_index.route('Med/<medication>', methods=['GET', 'POST'])
 @login_required
 def med(medication):
-	if medication in getMeds():
-		return render_template('med.html', med=medication)
-	else:
+
+	if medication not in getMeds():
 		return render_template('404.html')
 
+	form = RegistrationForm(request.form)
+
+	if request.method == 'POST' and form.validate():
+		return render_template('mymeds.html')
+
+	return render_template('med.html', med=medication, form=form)
+
+# display the meds that the user has registered
 @mod_index.route('mymeds')
 @login_required
 def mymeds():
 	return render_template('mymeds.html')
+
+# The Form that is filled to register new meds
+class RegistrationForm(Form):
+    amount = IntegerField('Amount', [validators.required(), validators.NumberRange(min=1)])
+    intake = SelectField(u'Intake', choices=[('1', 'Daily'), ('2', 'Every other day'), ('3', 'Weekly'), ('4', 'By need')])
+    notes = TextField('Notes', [validators.optional()])
